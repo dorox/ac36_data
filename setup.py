4e4a525b233f1e16682cb7b499a71fc352fb6cc5
ilia <dorox@ya.ru>
2021-01-15 18:56:29 +0300

    -

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name="ac36data",
     version="0.1",
     packages=["ac36data"],
-    package_data={"": ["./*"]},
+    package_dir={"ac36data": "ac36data"},
+    package_data={"": ["./ac36data/*"]},
 )
